cbb9c92620a35a1979ef9f94d42d6cff91e33946
Althis974 (Romeo Lossy) <rlossy@student.42.fr>
2020-06-23 13:56:58 +0200

    Create main.cpp

---
+++ srcs/main.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "../includes/Lexer.hpp"
+
+int     ft_atoi(const std::string &s)
+{
+	std::stringstream	stringStream;
+	int					nb;
+
+	stringStream << s;
+	stringStream >> nb;
+
+	return (nb);
+}
+
+double  ft_atof(const std::string &s)
+{
+	std::stringstream	stringStream;
+	double				nb;
+
+	stringStream << s;
+	stringStream >> nb;
+
+	return (nb);
+}
+
+int		main(int ac, char **av)
+{
+	try
+	{
+		Lexer           lexer;
+		std::string     line;
+
+		if (ac == 2)
+		{
+			std::ifstream file(av[1], std::ios::in);
+			if (!file)
+				throw Exception("File not found");
+
+			while (getline(file, line))
+				lexer.fill(line);
+
+			file.close();
+			lexer.execute();
+		}
+		else
+		{
+			do
+			{
+				getline(std::cin, line, '\n');
+				lexer.fill(line);
+			} while (line.find(";;") == line.npos);
+			lexer.execute();
+		}
+	}
+	catch (const Exception &e)
+	{
+		std::cerr << "ERROR : " << e.what() << std::endl;
+	}
+
+	return (0);
+}
+
