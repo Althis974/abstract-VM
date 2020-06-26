7d901d2720c22f2049c46a4a96f3f5431c578288
Althis974 (Romeo Lossy) <rlossy@student.42.fr>
2020-06-26 11:00:13 +0200

    Update Exception.cpp

--- srcs/Exception.cpp
+++ srcs/Exception.cpp
@@ -6,65 +6,65 @@
 
 const char *	Exception::OverflowException::what() const throw()
 {
-	return ("Exception: OverflowException on a value!");
+	return ("OverflowException: Overflow on a value!");
 }
 
 const char *	Exception::UnderflowException::what() const throw()
 {
-	return ("Exception: Underflow on a value!");
+	return ("UnderflowException: Underflow on a value!");
 }
 
 const char *	Exception::SyntaxException::what() const throw()
 {
-	return ("Exception: Syntax error!");
+	return ("SyntaxException: Syntax error!");
 }
 
 const char *	Exception::UnknownInstructionException::what() const throw()
 {
-	return ("Exception: Unknown instruction!");
+	return ("UnknownInstructionException: Unknown instruction!");
 }
 
 const char *	Exception::PopOnEmptyStackException::what() const throw()
 {
-	return ("Exception: Try to pop on an empty stack!");
+	return ("PopOnEmptyStackException: Try to pop on an empty stack!");
 }
 
 const char *	Exception::FloatingPointException::what() const throw()
 {
-	return ("Exception: Division/Modulo by 0!");
+	return ("FloatingPointException: Division/Modulo by 0!");
 }
 
 const char *	Exception::MissingExitInstructionException::what() const throw()
 {
-	return ("Exception: Missing exit instruction!");
+	return ("MissingExitInstructionException: Missing exit instruction!");
 }
 
 const char *	Exception::AssertException::what() const throw()
 {
-	return ("Exception: Assert instruction is not true!");
+	return ("AssertException: Assert instruction is not true!");
 }
 
 const char *	Exception::OperationOnEmptyStackException::what() const throw()
 {
-	return ("Exception: Operation failed because the stack had less than two operands!");
+	return ("OperationOnEmptyStackException: Operation failed because the stack had less than two operands!");
 }
 
 const char *	Exception::ModuloOnFloatingPointException::what() const throw()
 {
-	return ("Exception: Modulo impossible on floating point!");
+	return ("ModuloOnFloatingPointException: Modulo impossible on floating point!");
 }
 
 const char *	Exception::PrintNonAsciiException::what() const throw()
 {
-	return ("Exception: Impossible to print non ascii character!");
+	return ("PrintNonAsciiException: Impossible to print non ascii character!");
 }
 
 const char *	Exception::PrintOnEmptyStackException::what() const throw()
 {
-	return ("Exception: Impossible to print on empty stack!");
+	return ("PrintOnEmptyStackException: Impossible to print on empty stack!");
 }
 
 const char *	Exception::FileDoesNotExistException::what() const throw()
 {
-	return ("Exception: File doesn't exist!");
+	return ("FileDoesNotExistException: File doesn't exist!");
 }
