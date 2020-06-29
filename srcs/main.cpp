dd3e86fad35d4e99b3d63a5eab631e25ad0841e7
LOSSY Roméo <rlossy@student.42.fr>
2020-06-29 10:02:00 +0200

    Add 42 headers

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,29 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlossy <rlossy@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/29 09:47:21 by rlossy            #+#    #+#             */
+/*   Updated: 2020/06/29 09:53:19 by rlossy           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
 #include <iostream>
-#include "../includes/Parser.hpp"
-#include "../includes/Computer.hpp"
+#include <fstream>
+#include "../includes/Lexer.hpp"
+
+int     ft_atoi(const std::string &s)
+{
+	std::stringstream	stringStream;
+	int					nb;
+
+	stringStream << s;
+	stringStream >> nb;
+
+	return (nb);
+}
+
+double  ft_atof(const std::string &s)
+{
+	std::stringstream	stringStream;
+	double				nb;
+
+	stringStream << s;
+	stringStream >> nb;
+
+	return (nb);
+}
 
 int		main(int ac, char **av)
 {
 	try
 	{
+		Lexer           lexer;
+		std::string     line;
+
 		if (ac == 2)
 		{
-			Parser		parse1{};
-			Computer	comp1(parse1.readFile(av[1]));
+			std::ifstream file(av[1], std::ios::in);
+			if (!file)
+				throw Exception("File not found");
 
-			comp1.execute();
+			while (getline(file, line))
+				lexer.fill(line);
+
+			file.close();
+			lexer.execute();
 		}
-		else if (ac == 1)
+		else
 		{
-			Parser		parse2{};
-			Computer	comp2(parse2.readStdin());
-
-			comp2.execute();
+			do
+			{
+				getline(std::cin, line, '\n');
+				lexer.fill(line);
+			} while (line.find(";;") == line.npos);
+			lexer.execute();
 		}
 	}
-	catch (std::exception &e)
+	catch (const Exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cerr << "ERROR : " << e.what() << std::endl;
 	}
 
 	return (0);
